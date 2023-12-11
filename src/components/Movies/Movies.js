0f1e98ed6973f87d0a02cfa0f982621a68e52514
Eynulla Mirzezade <eynullamirzezade@gmail.com>
2023-12-11 13:04:28 +0400

    change

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -4,10 +4,38 @@ import "./Movies.css";
 import { connect } from "react-redux";
 
 class Movies extends Component {
+
+  state = { 
+    movies: [
+        {
+            imdbID: 'tt3896198',
+            Title: "Guardians of the Galaxy Vol. 2",
+            year: 2017,
+            Poster: "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"
+
+        },
+        {
+            imdbID: 'tt0068646',
+            Title: "The Godfather",
+            year: 1972,
+            Poster: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+
+        }
+    ]
+} 
+componentDidUpdate =(prevProps)=>{
+  if(prevProps.movies!=this.props.movies){
+  this.setState({
+    movies: this.props.movies
+  })
+ 
+  }
+
+}
   render() {
     return (
       <ul className="movies">
-        {this.props.movies.map((movie) => (
+        {this.state.movies.map((movie) => (
           <li className="movies__item" key={movie.imdbID}>
             <MovieItem {...movie} />
           </li>
